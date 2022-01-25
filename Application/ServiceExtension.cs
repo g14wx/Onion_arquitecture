b8066154b28b7b5f07aaf8d801fc5c06c39b00d9
dev11-dccolorweb <92184908+dev11-dccolorweb@users.noreply.github.com>
2022-01-24 22:34:09 -0600

    :sparkles: adding entrypoint of the app

---
+++ Application/ServiceExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+/*
+ * This class will help us to group extensions of third parties services or that ones made by us
+ * for example, if be want to register autoMapper then here is where it will be register in our service
+ * 
+ */
+public static class ServiceExtension
+{
+    /**
+     * Static method to register a service to our app
+     */
+    public static void AddApplicationLayer(IServiceCollection services)
+    {
+        // registing autoMapper to the service
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    }
+}
